ddbf99b91b9bb3bdc47f7fb4fddfa0fafef3f5d0
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-11-04 20:58:36 +0200

    [New]: Fixed errors for React.

--- src/app/components/FooterClientComponent.tsx
+++ src/app/components/FooterClientComponent.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import { useState } from "react"
 
@@ -6,29 +6,29 @@ export default function FooterClientComponent() {
 
     const footerNavs = [
         {
-            href: 'javascript:void()',
-            name: 'About'
+            href: "javascript:void()",
+            name: "About"
         },
         {
-            href: 'javascript:void()',
-            name: 'Blog'
+            href: "javascript:void()",
+            name: "Blog"
         },
         {
-            href: 'javascript:void()',
-            name: ''
+            href: "javascript:void()",
+            name: ""
         },
         {
-            href: 'javascript:void()',
-            name: 'Team'
+            href: "javascript:void()",
+            name: "Team"
         },
         {
-            href: 'javascript:void()',
-            name: 'Careers'
+            href: "javascript:void()",
+            name: "Careers"
         },
 
         {
-            href: 'javascript:void()',
-            name: 'Suuport'
+            href: "javascript:void()",
+            name: "Suuport"
         }
     ]
 
@@ -37,13 +37,13 @@ export default function FooterClientComponent() {
             <div className="max-w-lg sm:mx-auto sm:text-center">
                 <img src="https://www.floatui.com/logo.svg" className="w-32 sm:mx-auto" />
                 <p className="leading-relaxed mt-2 text-[15px]">
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
             </div>
             <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
                 {
                     footerNavs.map((item, idx) => (
-                        <li className=" hover:text-gray-800">
+                        <li key="idx" className=" hover:text-gray-800">
                             <a key={idx} href={item.href}>
                                 {item.name}
                             </a>
