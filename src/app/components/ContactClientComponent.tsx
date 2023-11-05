384baea2190d796707b751c8a49b201021beb0b3
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-11-05 10:05:37 +0200

    [New]: Fixed the texts and added a few images.

--- src/app/components/ContactClientComponent.tsx
+++ src/app/components/ContactClientComponent.tsx
@@ -95,14 +95,14 @@ export default function ContactClientComponent() {
                             <div className="relative mt-2">
                                 <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2">
                                     <select className="text-sm bg-transparent outline-none rounded-lg h-full">
-                                        <option>US</option>
-                                        <option>ES</option>
-                                        <option>MR</option>
+                                        <option>SA</option>
+                                        {/* <option>ES</option>
+                                        <option>MR</option> */}
                                     </select>
                                 </div>
                                 <input
                                     type="number"
-                                    placeholder="+1 (555) 000-000"
+                                    placeholder="+27 (555) 000-000"
                                     required
                                     className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                                 />
